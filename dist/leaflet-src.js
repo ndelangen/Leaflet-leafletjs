482c94386b5c2b4a17ad20ede4579e4df45c2313
Norbert de Langen <norbert@macaw.nl>
2016-05-18 13:53:11 +0200

    v1.8.3

--- dist/leaflet-src.js
+++ dist/leaflet-src.js
@@ -1362,7 +1362,7 @@ L.LatLngBounds.prototype = {
 //TODO International date line?
 
 L.latLngBounds = function (a, b) { // (LatLngBounds) or (LatLng, LatLng)
-	if (!a || a instanceof L.LatLngBounds) {
+	if (!a || a._northEast && a._southWest) {
 		return a;
 	}
 	return new L.LatLngBounds(a, b);
